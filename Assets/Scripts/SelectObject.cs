4455cdd65fe68f19eff55bc1d35e14aa09d135f7
bleedinglight <halpin2003@gmail.com>
2025-03-04 17:14:08 +0000

    initial camera stuff + tilemap + selection

---
+++ Assets/Scripts/SelectObject.cs
@@ -0,0 +1,54 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SelectObject : MonoBehaviour
+{
+    
+    private Camera cam;
+    private GameObject currentObject;
+    
+    [SerializeField] private LayerMask gameLayer;
+
+    void Awake()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) 
+        {
+
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            Debug.Log("fired");
+
+            if (Physics.Raycast(ray, out hit, 100, gameLayer)) 
+            {
+                
+                if(currentObject != null) 
+                {
+                    Destroy(currentObject.GetComponent<Outline>());
+                }
+
+                currentObject = hit.transform.gameObject;
+                currentObject.AddComponent<Outline>();
+
+            }
+
+            if(!Physics.Raycast(ray, out hit, 100, gameLayer))
+            {
+                if (currentObject != null) {
+                    Destroy(currentObject.GetComponent<Outline>());
+                    currentObject = null;
+                }
+            }
+
+           
+
+            }
+
+        }
+
+}
