66d422976dd6c1c654a065a2b1ae424be419eef2
Seatwentytwo <123640807+Seatwentytwo@users.noreply.github.com>
2025-03-09 23:28:42 +0000

    UI Basic Functionality
    Gonna start trying to make the Catalogue itself functional

---
+++ Assets/UI/activateCatalogue.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class activateCatalogue : MonoBehaviour
+{
+    public GameObject catalogueMenu;
+    private bool catalogueOpened = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (catalogueOpened == true)
+        {
+            if (Input.GetKeyUp(KeyCode.Escape))
+            {
+                for (int i = 0; i < catalogueMenu.transform.childCount; i++)
+                {
+                    catalogueMenu.transform.GetChild(i).gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+
+    public void openClose()
+    {
+        for (int i = 0; i < catalogueMenu.transform.childCount; i++)
+        {
+            catalogueMenu.transform.GetChild(i).gameObject.SetActive(true);
+        }
+
+        catalogueOpened = true;
+    }
+}
