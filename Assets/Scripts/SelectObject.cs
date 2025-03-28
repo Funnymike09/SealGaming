1a0047cb02766b223d909bfc86f50a2d54cf3930
bleedinglight <halpin2003@gmail.com>
2025-03-28 17:32:18 +0000

    seal moving and returnign

--- Assets/Scripts/SelectObject.cs
+++ Assets/Scripts/SelectObject.cs
@@ -6,19 +6,23 @@ public class SelectObject : MonoBehaviour
     
     private Camera cam;
     private GameObject currentObject;
+    private bool isSealBeingMoved;
+    private Vector3 returnPosition;
     
     [SerializeField] private LayerMask gameLayer;
+    [SerializeField] private LayerMask moveLayer;
 
     void Awake()
     {
         cam = Camera.main;
+        isSealBeingMoved = false;
     }
 
     void Update()
     {
         if (GridManager.instance.isBuildingBeingPlaced) return;
     
-        if(Input.GetMouseButtonDown(0)) 
+        if(Input.GetMouseButtonDown(0) && !isSealBeingMoved) 
         {
 
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
@@ -33,6 +37,7 @@ public class SelectObject : MonoBehaviour
                 }
 
                 currentObject = hit.transform.gameObject;
+                returnPosition = hit.transform.position;
                 if(currentObject.GetComponent<Outline>() == null) 
                 {
                     currentObject.AddComponent<Outline>();
@@ -53,9 +58,42 @@ public class SelectObject : MonoBehaviour
                 Destroy(currentObject.GetComponent<Outline>());
                 currentObject = null;
             }
+            
+            
 
         }
-
+        
+        if (currentObject == null) return;
+        if(currentObject.layer == LayerMask.NameToLayer("Seal")) 
+        {
+            SealMove();
+        }
+    }
+    
+    void SealMove() 
+    {
+        isSealBeingMoved = true;    
+        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        
+        Cursor.visible = false;
+        
+        if(Physics.Raycast(ray, out hit, 100, gameLayer)) 
+        {
+            Vector3 adjustedPosition = hit.transform.position;
+            adjustedPosition.y = adjustedPosition.y + 0.5f;
+            currentObject.transform.position = adjustedPosition;
+        }
+        
+        if(Input.GetMouseButtonDown(1)) 
+        {
+            currentObject.transform.position = returnPosition;
+            isSealBeingMoved = false;
+            Destroy(currentObject.GetComponent<Outline>());
+            currentObject = null;
+            Cursor.visible = true;
+        }
     }
 
 }
