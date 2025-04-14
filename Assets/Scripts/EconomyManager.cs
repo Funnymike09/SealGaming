b53f08751031cb802d701cbaffc5f932495a2d1b
Funnymike09 <mike.nip@mail.ru>
2025-04-14 08:26:51 +0500

    QOL for pop up amount

--- Assets/Scripts/EconomyManager.cs
+++ Assets/Scripts/EconomyManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
@@ -14,8 +15,15 @@ public class EconomyManager : MonoBehaviour
 
     public float currentMoney { get; private set; }
     public float currentWorkPower { get; private set; }
-    public float currentEnergy { get; private set ; }
+    public float currentEnergy { get; private set; }
     public float currentInfluence { get; private set; }
+
+    [Header("Popup text variables")]
+    public GameObject textPopupPrefab;
+    [SerializeField] private Transform popupParent;
+    public Vector3[] popupSpawnLocations = new Vector3[5]; // 0 = money, 1 = manpower, 2 = electro
+    public float textMoveSpeed;
+    public float stayTime;
 
 
     [Header("Starting Vaules")]
@@ -30,15 +38,15 @@ public class EconomyManager : MonoBehaviour
 
     void Awake()
     {
-        if (instance != null && instance != this) 
+        if (instance != null && instance != this)
         {
             Destroy(this);
         }
-        else 
+        else
         {
             instance = this;
         }
-        
+
         currentMoney = startingMoney;
         currentEnergy = startingEnergy;
         currentWorkPower = startingWorkPower;
@@ -52,49 +60,85 @@ public class EconomyManager : MonoBehaviour
 
     }
 
-    public void EnconomyTick() 
+    public void EnconomyTick()
     {
-        economyTickEvent.Invoke();    
+        economyTickEvent.Invoke();
         UpdateUI();
     }
 
-    public void UpdateUI() 
+    public void UpdateUI()
     {
         moneyText.text = currentMoney.ToString();
         workPowerText.text = currentWorkPower.ToString();
         energyText.text = currentEnergy.ToString();
         dayText.text = "Day: " + GridManager.instance.dayIndex;
     }
-    
-    public void AddMoney(float amount) 
+
+    public void AddMoney(float amount)
     {
         currentMoney += amount;
+        StartCoroutine(PopupText(popupSpawnLocations[0], amount.ToString(), Color.green, stayTime));
         //AudioManager.singleton.PlaySoundListOnce(gameObject, moner);
     }
-    
-    public void RemoveMoney(float amount) 
+
+    public void RemoveMoney(float amount)
     {
         currentMoney -= amount;
     }
-    
-    public void AddEnergy(float amount) 
+
+    public void AddEnergy(float amount)
     {
         currentEnergy += amount;
     }
-    
-    public void RemoveEnergy(float amount) 
+
+    public void RemoveEnergy(float amount)
     {
         currentEnergy -= amount;
     }
-    
-    public void AddWorkPower(float amount) 
+
+    public void AddWorkPower(float amount)
     {
         currentWorkPower += amount;
     }
-    
-    public void RemoveWorkPower(float amount) 
+
+    public void RemoveWorkPower(float amount)
     {
         currentWorkPower -= amount;
     }
 
+    public IEnumerator PopupText(Vector2 screenPos, string displayString, Color color, float stayTime) // create a popup showing how much money they earn / spend. QOL  // might need to add a empty game obj to each price tag 
+    {                                               
+        GameObject obj = Instantiate(textPopupPrefab, popupParent);
+        obj.GetComponent<RectTransform>().anchoredPosition = screenPos;
+        obj.transform.SetAsLastSibling();
+        TextMeshProUGUI objText = obj.GetComponent<TextMeshProUGUI>();
+        objText.text = displayString;
+        objText.color = color;
+
+        float timer = 0f;
+        while (timer < 1f)
+        {
+            obj.transform.position = new Vector2(obj.transform.position.x, obj.transform.position.y + textMoveSpeed * Time.deltaTime);
+            timer += Time.deltaTime;
+
+            if (obj.transform.position.y > 514)
+            {
+                timer = 1f;
+            }
+
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(stayTime);
+
+        //timer = 0f;
+        while (objText.color.a > 0)
+        {
+            objText.color = new Color(objText.color.r, objText.color.g, objText.color.b, objText.color.a - Time.deltaTime);
+            //timer += Time.deltaTime;
+            yield return null;
+        }
+        Destroy(obj);
+
+    }
 }
